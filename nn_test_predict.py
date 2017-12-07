21b09c741ef90c2a5e5a8a1210a4e6f9dd846a9a
iguanaus <johnpeurifoy@yahoo.com>
2017-12-07 08:02:54 -0500

    newnew

---
+++ nn_test_predict.py
@@ -0,0 +1,207 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__    import print_function
+
+
+from numpy import genfromtxt
+import numpy as np
+import argparse, os
+import tensorflow as tf
+from tensorflow.contrib.rnn.python.ops import *
+ 
+from tensorflow.contrib import rnn
+import matplotlib.pyplot as plt
+from tensorflow.contrib.rnn import BasicLSTMCell, BasicRNNCell, GRUCell
+import pandas as pd
+import re
+
+skipAmount = 10.0 #This is how frequent the data should be sampled.
+emaPredictInFuture = 50 #This is how much into the future it should have to predict
+seq_len = 50 #Sequence length.
+smoothedPrice = 200 #This just smooths the input. 
+reuse = True
+  
+#THis needs to take in the data, then return the data in a list of
+#The output should be in a np array form. Note that the y value doesn't have to be returned. 
+
+def file_data(filename="data/waveEqSim_A.csv"):
+    #filename="data/06_01_10.csv"
+    my_data = genfromtxt(filename, delimiter=',')
+    return my_data
+
+
+
+def main():
+    # --- Set data params ----------------
+    #Create Data
+    max_len_data = 1000000000
+
+    #data , standardDev,meanVal= file_data()
+    data2 = file_data()
+
+    n_input = len(data2[0])
+    print("N input length: " , n_input)
+
+    n_output = n_input
+    n_hidden = 100
+    learning_rate = 0.001
+    decay = 0.9
+    numEpochs = 200
+    #reuse = False
+
+    #Structure of this will be [weekday,seconds*1000,intPrice,volume]
+
+    X = tf.placeholder("float32",[None,seq_len,n_input])
+    Y = tf.placeholder("float32",[None,seq_len,n_input])
+
+    # Input to hidden layer
+    cell = None
+    h = None
+    num_layers = 3
+    #h_b = None
+    sequence_length = [seq_len] * 1
+
+    cell = BasicLSTMCell(n_hidden, state_is_tuple=True, forget_bias=1)
+
+    #cells = rnn.MultiRNNCell([cell] * num_layers, state_is_tuple=True)
+    cells = tf.contrib.rnn.MultiRNNCell([BasicLSTMCell(n_hidden, state_is_tuple=True, forget_bias =1) for _ in range(num_layers)],state_is_tuple=True)
+
+    if h == None:
+        h = cells.zero_state(1,tf.float32)
+
+    hidden_out, states = tf.nn.dynamic_rnn(cells, X, sequence_length=sequence_length, dtype=tf.float32,initial_state=h)
+
+
+    # Hidden Layer to Output
+    V_init_val = np.sqrt(6.)/np.sqrt(n_output + n_input)
+
+    V_weights = tf.get_variable("V_weights", shape = [n_hidden, n_output], \
+            dtype=tf.float32, initializer=tf.random_uniform_initializer(-V_init_val, V_init_val))
+    V_bias = tf.get_variable("V_bias", shape=[n_output], \
+            dtype=tf.float32, initializer=tf.constant_initializer(0.01))
+
+    hidden_out_list = tf.unstack(hidden_out, axis=1)
+    temp_out = tf.stack([tf.matmul(i, V_weights) for i in hidden_out_list])
+    output_data = tf.nn.bias_add(tf.transpose(temp_out, [1,0,2]), V_bias)
+
+
+    # define evaluate process
+    print("Output data: " , output_data)
+    print("Labels: " , Y)
+
+    cost = tf.reduce_sum(tf.square(Y-output_data))
+
+    #cost = tf.reduce_sum(tf.square(output_data-Y))
+    #correct_pred = tf.equal(tf.round(output_data*standardDev+meanVal), tf.round(Y*standardDev+meanVal))
+    #accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+
+    # --- Initialization ----------------------
+    global_step = tf.Variable(0, trainable=False)
+
+    learning_rate = tf.train.exponential_decay(learning_rate, global_step,
+                                           5000, 0.9, staircase=True)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate, decay=decay).minimize(cost)
+    init = tf.global_variables_initializer()
+
+    for i in tf.global_variables():
+        print(i.name)
+
+    step = 0
+    savename="models/"
+    if not os.path.exists(savename):
+        os.makedirs(savename)
+    saver = tf.train.Saver()
+    numFile = 0
+
+
+    with tf.Session(config=tf.ConfigProto(log_device_placement=False, allow_soft_placement=False)) as sess:
+        print("Session Created")
+        sess.run(init)
+        if reuse:
+            saver.restore(sess, os.path.join(savename,'my_model'))
+
+
+            #new_saver = tf.train.import_meta_graph("modelFile"+'.meta')
+            #new_saver.restore(sess, tf.train.latest_checkpoint('./'))
+
+        steps = []
+        losses = []
+        accs = []
+        validation_losses = []
+        curEpoch = 0
+
+        
+        training_state = None
+        i = 0
+        print ("Number train: " , len(data2))
+        train_file_name = "loss_val.csv"
+        train_loss_file = open(train_file_name,'w')
+        
+        outputList = np.array([[]])
+        desiredList = np.array([[]])
+
+        while i <= int(((len(data2)-1.0)/seq_len)-2):
+            i += 1
+            print("I: " , i)
+            myTrain_x = data2[seq_len*i:seq_len*(i+1)].reshape((1,seq_len,n_input))
+            myTrain_y = data2[seq_len*i+1:seq_len*(i+1)+1].reshape((1,seq_len,n_input))
+            #print("X Predict: " , myTrain_x)
+            myfeed_dict={X: myTrain_x, Y: myTrain_y}
+            if training_state is not None:
+                myfeed_dict[h] = training_state
+            
+            out_data,loss,training_state,output_data_2 = sess.run([output_data,cost,states,output_data], feed_dict = myfeed_dict)
+            
+            print("Y Values: " , myTrain_y[0])
+            
+            print("Output data: " , out_data[0])
+            
+            print("Epoch: " + str(curEpoch) + " Iter " + str(i) + ", Minibatch Loss= " + \
+                  "{:.6f}".format(loss))
+            outputVal = np.array(out_data[0])
+            correctVal = np.array(myTrain_y[0])
+            print("OutputVal: " , outputVal)
+            #outputVal = np.array(output_data_2*standardDev+meanVal)
+            #correctVal = myTrain_y*standardDev+meanVal
+            #Okay we need to write two columns to the file, one for outputVal, one for correctVal
+
+            #print("Output: " , outputVal[0])
+            print(len(outputList))
+
+            if len(outputList) == 1:
+                outputList = outputVal
+                print("OutputList within the if: " , outputList)
+                desiredList = correctVal
+            else:
+                print("Jumping to this....")
+                print("outputList: ")
+                print(outputList)
+                print("OutputVal: " )
+                print(outputVal)
+                outputList = np.append(outputList,outputVal,axis=0)
+                desiredList = np.append(desiredList,correctVal,axis=0)
+            #print(outputList)
+            #print(desiredList)
+            print("output list: " )
+            print(outputList)
+
+            #print("My train: " , correctVal)
+            #print("Output - myTrain: " , outputVal-correctVal)
+        print ("Output list: ")
+        print(outputList)
+        print("Desired List: ")
+        print(desiredList)
+
+        np.savetxt('y_predictList_vol500_price_long.csv', outputList, delimiter=',')
+        np.savetxt('y_actualList_vol500_price_long.csv', desiredList, delimiter=',')
+
+        train_loss_file.close()
+
+        print("Optimization Finished!")
+        
+
+
+if __name__=="__main__":
+    
+    main()
---
+++ nn_try_projectile.py
@@ -0,0 +1,283 @@
+'''
+    This program trains a feed-forward neural network. It takes in a geometric design (the radi of concentric spheres), and outputs the scattering spectrum. It is meant to be the first program run, to first train the weights. 
+'''
+
+import tensorflow as tf
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import os
+import time
+import argparse, os
+import random
+
+RANDOM_SEED = 42
+tf.set_random_seed(RANDOM_SEED)
+
+num_decay = 4320000
+
+def init_weights(shape):
+    """ Weight initialization """
+    weights = tf.random_normal(shape, stddev=.1)
+    return tf.Variable(weights)
+
+def init_bias(shape):
+    """ Weight initialization """
+    biases = tf.random_normal([shape], stddev=.1)
+    return tf.Variable(biases)
+
+def save_weights(weights,biases,output_folder,weight_name_save,num_layers):
+    print("Biases: " , biases, len(biases))
+    for i in xrange(0, num_layers+1):
+        weight_i = weights[i].eval()
+        np.savetxt(output_folder+weight_name_save+"w_"+str(i)+".txt",weight_i,delimiter=',')
+        if i != num_layers:
+            for j in xrange(0,5):
+                bias_i = biases[5*i+j].eval()
+                np.savetxt(output_folder+weight_name_save+"b_"+str(i)+"-"+str(j)+".txt",bias_i,delimiter=',')
+        else:
+            bias_i = biases[5*i].eval()
+            np.savetxt(output_folder+weight_name_save+"b_"+str(i)+".txt",bias_i,delimiter=',')
+        print("Bias: " , i, " : ", bias_i)
+    return
+
+def load_weights(output_folder,weight_load_name,num_layers):
+    weights = []
+    biases = []
+    #Fine if everything is 2D
+    for i in xrange(0, num_layers+1):
+        weight_i = np.loadtxt(output_folder+weight_load_name+"w_"+str(i)+".txt",delimiter=',')
+        w_i = tf.Variable(weight_i,dtype=tf.float32)
+        weights.append(w_i)
+        if i != num_layers:
+            for j in xrange(0,5):
+                bias_i = np.loadtxt(output_folder+weight_load_name+"b_"+str(i)+"-"+str(j)+".txt",delimiter=',')
+                b_i = tf.Variable(bias_i,dtype=tf.float32)
+                biases.append(b_i)
+        else:
+            bias_i = np.loadtxt(output_folder+weight_load_name+"b_"+str(i)+".txt",delimiter=',')
+            b_i = tf.Variable(bias_i,dtype=tf.float32)
+            biases.append(b_i)
+
+        print("Last Bias: " , bias_i)
+    #If it needs to be one D
+    if True:
+        print("weights:")
+        print(weights)
+        weight_1 = np.reshape(np.loadtxt(output_folder+weight_load_name+"w_"+str(num_layers)+".txt",delimiter=','),(-1,1))
+        weight_new_1 = tf.Variable(weight_1,dtype=tf.float32)
+        weights[-1] = weight_new_1
+
+        bias_1 = np.reshape(np.loadtxt(output_folder+weight_load_name+"b_"+str(num_layers)+".txt",delimiter=','),(-1,1))
+        print("bias: " , bias_1)
+        bias_new_1 = tf.Variable(bias_1,dtype=tf.float32)
+        biases[-1] = bias_new_1
+
+
+        #print(weights)
+    print("biases: " , len(biases))
+    print("weights: " , len(weights))
+
+
+    return weights , biases
+
+def forwardprop(X, weights, biases, num_layers,dropout=False):
+    htemp = None
+    #I will preset the architecture.
+    htemp = tf.add(tf.nn.sigmoid(tf.matmul(X,weights[0])),biases[0])#Go into first 10 layer
+    htemp = tf.add(tf.nn.sigmoid(tf.matmul(htemp,weights[1])),biases[1]) #Second 10
+    htemp = tf.add(tf.nn.sigmoid(tf.matmul(htemp,weights[2])),biases[2])# Now 5
+    htemp = tf.add(tf.nn.sigmoid(tf.matmul(htemp,weights[3])),biases[3]) #Now third 10
+    htemp = tf.add(tf.nn.sigmoid(tf.matmul(htemp,weights[4])),biases[4]) #Now last layer
+    yval = tf.add(tf.matmul(htemp,weights[-1]),biases[-1])
+    print("Last bias: " , biases[-1])
+    return (yval)
+
+#This method reads from the 'X' and 'Y' file and gives in the input as an array of arrays (aka if the input dim is 5 and there are 10 training sets, the input is a 10X 5 array)
+#a a a a a       3 3 3 3 3 
+#b b b b b       4 4 4 4 4
+#c c c c c       5 5 5 5 5
+
+def get_data(percentTest=.2,random_state=42):
+    #Gen the x data
+    #Gen the y data
+    
+    #g = -.2394
+    num_entries = 100000
+    #min_val = 0.0
+    #max_val = 100.0
+    
+
+    x = np.zeros(shape=(num_entries,2))
+    y = np.zeros(shape=(num_entries,1))
+    for i in xrange(0,num_entries):
+        #I will feed in two timesteps of the data
+        y0 = random.uniform(0.0,.5)
+        v = random.uniform(0.0,.5)# These should be random. 
+        g = random.uniform(0.0,.5)
+        t = random.uniform(0.0,.5)
+        dt = random.uniform(0.0,.5)
+
+        x0 = t
+        x1 = y0 + v*t+-0.5*g*t*t
+        x2 = y0 + v*(t+dt)+-0.5*g*(t+dt)*(t+dt)
+        x3 = y0 + v*(t+2*dt)+-0.5*g*(t+2*dt)*(t+2*dt)
+
+        x[i] = [x1,x2]
+        y[i] = [x3]
+
+        #randNum1 = random.uniform(min_val, max_val)
+        #randNum2 = randNum1#random.uniform(min_val, max_val)
+        #x[i] = [randNum1,randNum2]
+        #y[i] = [randNum1*randNum2]
+    train_X = x
+    train_Y = y
+    print x
+    print y
+    X_train, X_val, y_train, y_val = train_test_split(train_X,train_Y,test_size=percentTest,random_state=random_state)
+    return X_train, y_train, X_val, y_val
+
+def main(reuse_weights,output_folder,weight_name_save,weight_name_load,n_batch,numEpochs,lr_rate,lr_decay,num_layers,n_hidden,percent_val):
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    train_X, train_Y , val_X, val_Y = get_data(percentTest=percent_val)
+
+    x_size = train_X.shape[1]
+    y_size = train_Y.shape[1]
+
+    # Symbols
+    X = tf.placeholder("float", shape=[None, x_size])
+    y = tf.placeholder("float", shape=[None, y_size])
+    weights = []
+    biases = []
+    coef = []
+    # Weight initializations
+    if reuse_weights:
+        (weights, biases) = load_weights(output_folder,weight_name_load,num_layers)
+
+    else:
+        weights.append(init_weights((x_size,10))) #First
+        biases.append(init_bias(10))
+        weights.append(init_weights((10,10))) #Second
+        biases.append(init_bias(10))
+        weights.append(init_weights((10,5)))   #Mid 5
+        biases.append(init_bias(5))
+        weights.append(init_weights((5,10))) #Third
+        biases.append(init_bias(10))
+        weights.append(init_weights((10,10))) #Fourth
+        biases.append(init_bias(10))
+        weights.append(init_weights((10,1))) #Out
+        biases.append(init_bias(1))
+
+    #for ele in coef:
+    #    biases.append(ele)
+    print("Coefficients")
+    print(biases)
+
+    # Forward propagation
+    yhat    = forwardprop(X, weights,biases,num_layers)
+    
+    # Backward propagation
+    cost = tf.reduce_sum(tf.square(y-yhat))
+
+    global_step = tf.Variable(0, trainable=False)
+    learning_rate = lr_rate
+    #learning_rate = tf.train.exponential_decay(lr_rate,global_step,num_decay,.96,staircase=False)
+
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate, decay=lr_decay).minimize(cost,global_step=global_step)
+
+    with tf.Session() as sess:
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        step = 0
+        curEpoch=0
+        cum_loss = 0
+        numFile = 0 
+        while True:
+            train_file_name = output_folder+"train_train_loss_" + str(numFile) + ".txt"
+            if os.path.isfile(train_file_name):
+                numFile += 1
+            else:
+                break
+        train_loss_file = open(train_file_name,'w')
+        val_loss_file = open(output_folder+"train_val_loss_"+str(numFile) + "_val.txt",'w')
+        start_time=time.time()
+        print("========                         Iterations started                  ========")
+        while curEpoch < numEpochs:
+            batch_x = train_X[step * n_batch : (step+1) * n_batch]
+            batch_y = train_Y[step * n_batch : (step+1) * n_batch]
+            #print(batch_x)
+            #print(batch_y)
+            sess.run(optimizer, feed_dict={X: batch_x, y: batch_y})
+            myvals, new_loss = sess.run([yhat,cost],feed_dict={X:batch_x,y:batch_y})
+            #print((myvals[0]-batch_y[0])/myvals[0]*100.0)
+            #print('\n')
+            #print(myvals[0])
+            #print(batch_y[0])
+            #print((myvals - batch_y)[0][0])
+            cum_loss += new_loss
+            step += 1
+            if step == int(train_X.shape[0]/n_batch): #Epoch finished
+                step = 0
+                curEpoch +=1            
+                train_loss_file.write(str(float(cum_loss))+str("\n"))
+                if (curEpoch % 10 == 0 or curEpoch == 1):
+                    #Calculate the validation loss
+                    val_loss = sess.run(cost,feed_dict={X:val_X,y:val_Y})
+                    print("Validation loss: " , str(val_loss))
+                    val_loss_file.write(str(float(val_loss))+str("\n"))
+                    val_loss_file.flush()
+
+                    print("Epoch: " + str(curEpoch+1) + " : Loss: " + str(cum_loss))
+                    train_loss_file.flush()
+                cum_loss = 0
+        save_weights(weights,biases,output_folder,weight_name_save,num_layers)
+    print "========Iterations completed in : " + str(time.time()-start_time) + " ========"
+    sess.close()
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(
+        description="Physics Net Training")
+    parser.add_argument("--reuse_weights",type=str,default='False')
+    parser.add_argument("--output_folder",type=str,default='results/Project_1')
+        #Generate the loss file/val file name by looking to see if there is a previous one, then creating/running it.
+    parser.add_argument("--weight_name_load",type=str,default="")#This would be something that goes infront of w_1.txt. This would be used in saving the weights
+    parser.add_argument("--weight_name_save",type=str,default="")
+    parser.add_argument("--n_batch",type=int,default=1000)
+    parser.add_argument("--numEpochs",type=int,default=50)
+    parser.add_argument("--lr_rate",default=.1)
+    parser.add_argument("--lr_decay",default=.9)
+    parser.add_argument("--num_layers",default=1)
+    parser.add_argument("--n_hidden",default=2)
+    parser.add_argument("--percent_val",default=.2)
+
+    args = parser.parse_args()
+    dict = vars(args)
+
+    for i in dict:
+        if (dict[i]=="False"):
+            dict[i] = False
+        elif dict[i]=="True":
+            dict[i] = True
+        
+    kwargs = {  
+            'reuse_weights':dict['reuse_weights'],
+            'output_folder':dict['output_folder'],
+            'weight_name_save':dict['weight_name_save'],
+            'weight_name_load':dict['weight_name_load'],
+            'n_batch':dict['n_batch'],
+            'numEpochs':dict['numEpochs'],
+            'lr_rate':dict['lr_rate'],
+            'lr_decay':dict['lr_decay'],
+            'num_layers':dict['num_layers'],
+            'n_hidden':dict['n_hidden'],
+            'percent_val':dict['percent_val']
+            }
+
+    main(**kwargs)
+
+
+
+
